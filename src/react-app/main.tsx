10af9ee67ab341dd693abf8d4c323b1aea47d633
Drop <pedro.drop.contato@gmail.com>
2026-06-27 22:54:00 -0300

    feat: integrar Lenis para scroll suave

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/react-app/index.css";
 import App from "@/react-app/App";
+import Lenis from "lenis";
+
+const lenis = new Lenis({
+  duration: 1.4,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smoothWheel: true,
+});
+
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
